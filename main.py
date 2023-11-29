9f3068e7d1371626b0440352a5e2c461dbbdd941
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-11-30 00:22:32 +0900

    added the extraction of the most commonly used words in the book

--- main.py
+++ main.py
@@ -21,4 +21,18 @@ word = input("Pick the word you want to know how many times it appears: ")
 word_pattern = re.compile(fr"[A-Z]{{1}}[^.]*[^a-zA-Z]+{word}[^a-zA-Z]+[^.]*.")
 result = re.findall(word_pattern, book)
 
-print(result)
+#Extract the 5 most commonly used words in the book
+pattern = re.compile("[a-zA-Z]+")
+result = re.findall(pattern, book.lower())
+result[:5]
+
+words ={}
+for word in result:
+    if word in words.keys():
+        words[word] = words[word] + 1
+    else:
+        words[word] = 1
+
+words_list = [{value, key} for (key, value) in words.items()]
+sorted(words_list, reverse=True)
+print(sorted(words_list)[:5])
